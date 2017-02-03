fea698e6ce676406c04518ae11ee19ebf1d808f5
Nikita Maltsev <nikitamalcev1@gmail.com>
2017-02-03 23:05:23 +0200

    Added clients

---
+++ WebApplication1/WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var aValue = int.Parse(TextBox1.Text);
+            var bValue = int.Parse(TextBox2.Text);
+
+            var client = new SummatorService.SummatorServiceClient("BasicHttpBinding_ISummatorService");
+
+            Label1.Text = client.GetSum(aValue, bValue).ToString();
+            client.Close();
+
+        }
+    }
+}
