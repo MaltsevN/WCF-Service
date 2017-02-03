eae555ca45a8a39d091e1927ca1647461f739805
Nikita Maltsev <nikitamalcev1@gmail.com>
2017-02-03 22:50:42 +0200

    Created

---
+++ WCF Service/SummatorService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "SummatorService" in both code and config file together.
+    public class SummatorService : ISummatorService
+    {
+        public int GetSum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
